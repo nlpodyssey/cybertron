fbaddb09cbe6e70487ba7d365ee414444dc12428
Juliano Viana <juliano.viana@zuva.ai>
2022-08-29 17:53:12 -0400

    refactoring bert converter to make control flow more explicit

--- cmd/converter/converter.go
+++ cmd/converter/converter.go
@@ -3,15 +3,25 @@ package main
 import (
 	"fmt"
 	"os"
+	"strconv"
 
 	"github.com/nlpodyssey/cybertron/pkg/converter"
 )
 
 func main() {
-	modelDir:=os.Args[1]
+	if len(os.Args) != 3 {
+		fmt.Printf("Usage: %s <model_path> <overwrite_if_exists>\n", os.Args[0])
+		os.Exit(1)
+	}
+	modelDir := os.Args[1]
+	overwriteIfExists, err := strconv.ParseBool(os.Args[2])
+	if err != nil {
+		fmt.Printf("Failed to parse overwrite_if_exists: %s\n", err)
+		os.Exit(1)
+	}
 	fmt.Printf("Converting model from dir %s\n", modelDir)
-	err:=converter.Convert[float32](modelDir,true)
-	if err!=nil{
+	err = converter.Convert[float32](modelDir, overwriteIfExists)
+	if err != nil {
 		panic(err)
 	}
 }

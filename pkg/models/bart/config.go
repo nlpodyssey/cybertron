dcf2c0fb093a7d166f89bd51f2a9809d49858f61
matteo-grella <matteogrella@gmail.com>
2022-10-03 14:42:34 +0200

    Use default values on loading the BART config file

--- pkg/models/bart/config.go
+++ pkg/models/bart/config.go
@@ -13,64 +13,64 @@ import (
 // Config contains the global configuration of the Bart model and the heads of fine-tuning tasks.
 // The configuration coincides with that of Hugging Face to facilitate compatibility between the two architectures.
 type Config struct {
-	NumLabels                  int               `json:"_num_labels"`
-	ActivationDropout          float64           `json:"activation_dropout"`
-	ActivationFunction         string            `json:"activation_function"`
-	BiasLogits                 bool              `json:"add_bias_logits"`
-	FinalLayerNorm             bool              `json:"add_final_layer_norm"`
-	Architecture               []string          `json:"architectures"`
-	AttentionDropout           float64           `json:"attention_dropout"`
-	BosTokenID                 int               `json:"bos_token_id"`
-	ClassifierDropout          float64           `json:"classif_dropout"`
-	DModel                     int               `json:"d_model"`
-	DecoderAttentionHeads      int               `json:"decoder_attention_heads"`
-	DecoderFFNDim              int               `json:"decoder_ffn_dim"`
-	DecoderLayerDrop           float64           `json:"decoder_layerdrop"`
-	DecoderLayers              int               `json:"decoder_layers"`
-	DecoderStartTokenID        int               `json:"decoder_start_token_id"`
-	Dropout                    float64           `json:"dropout"`
-	EncoderAttentionHeads      int               `json:"encoder_attention_heads"`
-	EncoderFFNDim              int               `json:"encoder_ffn_dim"`
-	EncoderLayerDrop           float64           `json:"encoder_layerdrop"`
-	EncoderLayers              int               `json:"encoder_layers"`
-	EosTokenID                 int               `json:"eos_token_id"`
-	FineTuningTask             string            `json:"finetuning_task"`
-	ForceBosTokenToBeGenerated bool              `json:"force_bos_token_to_be_generated"`
-	ID2Label                   map[string]string `json:"id2label"`
-	InitStd                    float64           `json:"init_std"`
-	IsEncoderDecoder           bool              `json:"is_encoder_decoder"`
-	Label2ID                   map[string]int    `json:"label2id"`
-	LengthPenalty              float64           `json:"length_penalty"`
-	MaxPositionEmbeddings      int               `json:"max_position_embeddings"`
-	ModelType                  string            `json:"model_type"`
-	NormalizeBefore            bool              `json:"normalize_before"`
-	NormalizeEmbedding         bool              `json:"normalize_embedding"`
-	NumHiddenLayers            int               `json:"num_hidden_layers"`
-	OutputPast                 bool              `json:"output_past"`
-	PadTokenID                 int               `json:"pad_token_id"`
-	ScaleEmbedding             bool              `json:"scale_embedding"`
-	StaticPositionEmbeddings   bool              `json:"static_position_embeddings"`
-	TotalFlos                  float64           `json:"total_flos"`
-	VocabSize                  int               `json:"vocab_size"`
-	NumBeams                   int               `json:"num_beams"`
-	MaxLength                  int               `json:"max_length"`
-	MinLength                  int               `json:"min_length"`
-	BadWordsIDs                [][]int           `json:"bad_words_ids"`
-	EarlyStopping              bool              `json:"early_stopping"`
-	NoRepeatNGramSize          int               `json:"no_repeat_ngram_size"`
-	ExtraSpecialTokens         map[int]string    `json:"extra_special_tokens"`
+	NumLabels                  int               `json:"_num_labels,omitempty"`
+	ActivationDropout          float64           `json:"activation_dropout,omitempty"`
+	ActivationFunction         string            `json:"activation_function,omitempty"`
+	BiasLogits                 bool              `json:"add_bias_logits,omitempty"`
+	FinalLayerNorm             bool              `json:"add_final_layer_norm,omitempty"`
+	Architecture               []string          `json:"architectures,omitempty"`
+	AttentionDropout           float64           `json:"attention_dropout,omitempty"`
+	BosTokenID                 int               `json:"bos_token_id,omitempty"`
+	ClassifierDropout          float64           `json:"classif_dropout,omitempty"`
+	DModel                     int               `json:"d_model,omitempty"`
+	DecoderAttentionHeads      int               `json:"decoder_attention_heads,omitempty"`
+	DecoderFFNDim              int               `json:"decoder_ffn_dim,omitempty"`
+	DecoderLayerDrop           float64           `json:"decoder_layerdrop,omitempty"`
+	DecoderLayers              int               `json:"decoder_layers,omitempty"`
+	DecoderStartTokenID        int               `json:"decoder_start_token_id,omitempty"`
+	Dropout                    float64           `json:"dropout,omitempty"`
+	EncoderAttentionHeads      int               `json:"encoder_attention_heads,omitempty"`
+	EncoderFFNDim              int               `json:"encoder_ffn_dim,omitempty"`
+	EncoderLayerDrop           float64           `json:"encoder_layerdrop,omitempty"`
+	EncoderLayers              int               `json:"encoder_layers,omitempty"`
+	EosTokenID                 int               `json:"eos_token_id,omitempty"`
+	FineTuningTask             string            `json:"finetuning_task,omitempty"`
+	ForceBosTokenToBeGenerated bool              `json:"force_bos_token_to_be_generated,omitempty"`
+	ID2Label                   map[string]string `json:"id2label,omitempty"`
+	InitStd                    float64           `json:"init_std,omitempty"`
+	IsEncoderDecoder           bool              `json:"is_encoder_decoder,omitempty"`
+	Label2ID                   map[string]int    `json:"label2id,omitempty"`
+	LengthPenalty              float64           `json:"length_penalty,omitempty"`
+	MaxPositionEmbeddings      int               `json:"max_position_embeddings,omitempty"`
+	ModelType                  string            `json:"model_type,omitempty"`
+	NormalizeBefore            bool              `json:"normalize_before,omitempty"`
+	NormalizeEmbedding         bool              `json:"normalize_embedding,omitempty"`
+	NumHiddenLayers            int               `json:"num_hidden_layers,omitempty"`
+	OutputPast                 bool              `json:"output_past,omitempty"`
+	PadTokenID                 int               `json:"pad_token_id,omitempty"`
+	ScaleEmbedding             bool              `json:"scale_embedding,omitempty"`
+	StaticPositionEmbeddings   bool              `json:"static_position_embeddings,omitempty"`
+	TotalFlos                  float64           `json:"total_flos,omitempty"`
+	VocabSize                  int               `json:"vocab_size,omitempty"`
+	NumBeams                   int               `json:"num_beams,omitempty"`
+	MaxLength                  int               `json:"max_length,omitempty"`
+	MinLength                  int               `json:"min_length,omitempty"`
+	BadWordsIDs                [][]int           `json:"bad_words_ids,omitempty"`
+	EarlyStopping              bool              `json:"early_stopping,omitempty"`
+	NoRepeatNGramSize          int               `json:"no_repeat_ngram_size,omitempty"`
+	ExtraSpecialTokens         map[int]string    `json:"extra_special_tokens,omitempty"`
 	Cybertron                  struct {
-		Training                           bool   `json:"training"`
-		PositionalEncoderOffset            int    `json:"positional_encoder_offset"`
-		SharedEmbeddingsStoreName          string `json:"shared_embeddings_store_name"`
-		DecoderPositionalEncodingStoreName string `json:"decoder_positional_encoding_store_name"`
-		EncoderPositionalEncodingStoreName string `json:"encoder_positional_encoding_store_name"`
+		Training                           bool   `json:"training,omitempty"`
+		PositionalEncoderOffset            int    `json:"positional_encoder_offset,omitempty"`
+		SharedEmbeddingsStoreName          string `json:"shared_embeddings_store_name,omitempty"`
+		DecoderPositionalEncodingStoreName string `json:"decoder_positional_encoding_store_name,omitempty"`
+		EncoderPositionalEncodingStoreName string `json:"encoder_positional_encoding_store_name,omitempty"`
 	}
 }
 
 // ConfigFromFile loads a Bart model Config from file.
 func ConfigFromFile(file string) (Config, error) {
-	var config Config
+	config := baseConfig()
 	configFile, err := os.Open(file)
 	if err != nil {
 		return Config{}, err
@@ -81,6 +81,12 @@ func ConfigFromFile(file string) (Config, error) {
 		return Config{}, err
 	}
 	return config, nil
+}
+
+func baseConfig() Config {
+	return Config{
+		NormalizeEmbedding: true,
+	}
 }
 
 // EntailmentID returns the id of the `entailment` labels.
